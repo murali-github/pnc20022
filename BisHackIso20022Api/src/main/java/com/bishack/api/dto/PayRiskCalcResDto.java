bf481199fbfe3693f69cb29d5c979d9ead1a12d3
Kian Nejadfard <k.nejadfard@gmail.com>
2021-03-15 17:29:02 -0400

    Add response DTO for pay risk calc api and dummy rest endpoint

---
+++ BisHackIso20022Api/src/main/java/com/bishack/api/dto/PayRiskCalcResDto.java
@@ -0,0 +1,22 @@
+package com.bishack.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PayRiskCalcResDto {
+	@JsonProperty("riskFactor")
+	private String riskFactor;
+
+	@JsonProperty("riskFactor")
+	public String getRiskFactor() {
+		return riskFactor;
+	}
+
+	@JsonProperty("riskFactor")
+	public void setRiskFactor(String riskFactor) {
+		this.riskFactor = riskFactor;
+	}
+}
