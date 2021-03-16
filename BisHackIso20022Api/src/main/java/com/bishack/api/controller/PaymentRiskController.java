12ffe20e8c5b641228435ad064ccbeb85ca93f6d
Kian Nejadfard <k.nejadfard@gmail.com>
2021-03-16 09:32:50 -0400

    Add @ResponseBody annotation

--- BisHackIso20022Api/src/main/java/com/bishack/api/controller/PaymentRiskController.java
+++ BisHackIso20022Api/src/main/java/com/bishack/api/controller/PaymentRiskController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bishack.api.dto.PayRiskCalcReqDto;
 import com.bishack.api.dto.PayRiskCalcResDto;
@@ -18,6 +19,7 @@ public class PaymentRiskController {
 	private AppProperties appProperties;
 
 	@PostMapping(path = "/paymentRisk", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
 	public ResponseEntity<PayRiskCalcResDto> calcPaymentRisk(@RequestBody PayRiskCalcReqDto reqDto) {
 		// TODO make calls to pre-validation APIs here and check if inputs are correct.
 		PayRiskCalcResDto resDto = new PayRiskCalcResDto();
