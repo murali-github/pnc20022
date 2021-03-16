7105f901d936d1a3adc01a6c4862ba0256af3957
Kian Nejadfard <k.nejadfard@gmail.com>
2021-03-16 12:02:20 -0400

    Replace customer with TxRecord to represent data model

--- BisHackIso20022Api/src/main/java/com/bishack/api/controller/PaymentRiskController.java
+++ BisHackIso20022Api/src/main/java/com/bishack/api/controller/PaymentRiskController.java
@@ -1,5 +1,7 @@
 package com.bishack.api.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,19 +13,48 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bishack.api.dto.PayRiskCalcReqDto;
 import com.bishack.api.dto.PayRiskCalcResDto;
+import com.bishack.api.entity.TxRecord;
+import com.bishack.api.service.IPersistenceService;
+import com.bishack.api.service.SwiftApiService;
 import com.bishack.config.AppProperties;
 
 @Controller
 public class PaymentRiskController {
 	@Autowired
 	private AppProperties appProperties;
+	
+	@Autowired
+	private SwiftApiService swiftService;
+	
+	@Autowired
+	private IPersistenceService persistenceService;
 
 	@PostMapping(path = "/paymentRisk", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<PayRiskCalcResDto> calcPaymentRisk(@RequestBody PayRiskCalcReqDto reqDto) {
-		// TODO make calls to pre-validation APIs here and check if inputs are correct.
-		PayRiskCalcResDto resDto = new PayRiskCalcResDto();
-		resDto.setRiskFactor("HIGH");
-		return new ResponseEntity<PayRiskCalcResDto>(resDto, HttpStatus.OK);
+		PayRiskCalcResDto resDto = null;
+		try {
+			// Only accept this hard-coded source account
+			if (reqDto.getCreditorAccount() != "123110040000109876543210") {
+				return new ResponseEntity<PayRiskCalcResDto>(resDto, HttpStatus.BAD_REQUEST);
+			}
+			
+			TxRecord record = new TxRecord();
+			record.setCreditorName(reqDto.getCreditorName());
+			record.setCreditorAccount(reqDto.getCreditorAccount());
+			record.setInstitutionName(reqDto.getInstitutionName());
+			record.setUuid(UUID.randomUUID().toString());
+			record.setAmount(reqDto.getAmount());
+			// TODO add prevalidation result stuff to record
+			TxRecord savedRecord = persistenceService.saveRecord(record);
+
+			// TODO this should come from RiskEngine later when it's implemented
+			resDto = new PayRiskCalcResDto();
+			resDto.setRiskFactor("HIGH");
+			return new ResponseEntity<PayRiskCalcResDto>(resDto, HttpStatus.OK);
+		}
+		catch (Exception e) {
+			return new ResponseEntity<PayRiskCalcResDto>(resDto, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
