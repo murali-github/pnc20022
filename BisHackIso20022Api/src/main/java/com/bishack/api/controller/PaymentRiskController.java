936857cb560299dc3cbc7f04d0564c837734d3f7
Kian Nejadfard <k.nejadfard@gmail.com>
2021-03-16 09:25:29 -0400

    Rename to PaymentRiskController

--- BisHackIso20022Api/src/main/java/com/bishack/api/controller/PaymentRiskController.java
+++ BisHackIso20022Api/src/main/java/com/bishack/api/controller/PaymentRiskController.java
@@ -13,11 +13,11 @@ import com.bishack.api.dto.PayRiskCalcResDto;
 import com.bishack.config.AppProperties;
 
 @Controller
-public class PayRiskCalcController {
+public class PaymentRiskController {
 	@Autowired
 	private AppProperties appProperties;
 
-	@PostMapping(path = "/calcPaymentRisk", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(path = "/paymentRisk", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<PayRiskCalcResDto> calcPaymentRisk(@RequestBody PayRiskCalcReqDto reqDto) {
 		// TODO make calls to pre-validation APIs here and check if inputs are correct.
 		PayRiskCalcResDto resDto = new PayRiskCalcResDto();
