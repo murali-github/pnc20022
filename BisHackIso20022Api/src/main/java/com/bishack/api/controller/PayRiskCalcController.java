31e3d38a23c0580215966b1629117d0f00940543
Kian Nejadfard <k.nejadfard@gmail.com>
2021-03-16 09:24:16 -0400

    Implement basic mocked paymentRisk endpoint

--- BisHackIso20022Api/src/main/java/com/bishack/api/controller/PayRiskCalcController.java
+++ BisHackIso20022Api/src/main/java/com/bishack/api/controller/PayRiskCalcController.java
@@ -2,6 +2,7 @@ package com.bishack.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,19 +10,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.bishack.api.dto.PayRiskCalcReqDto;
 import com.bishack.api.dto.PayRiskCalcResDto;
-import com.bishack.api.entity.Customer;
-import com.bishack.api.service.IPersistenceService;
 import com.bishack.config.AppProperties;
 
 @Controller
 public class PayRiskCalcController {
 	@Autowired
 	private AppProperties appProperties;
-  
-    @PostMapping(path="/calcPaymentRisk", headers={"Accept=application/json", "Content-Type=application/json"})
+
+	@PostMapping(path = "/calcPaymentRisk", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<PayRiskCalcResDto> calcPaymentRisk(@RequestBody PayRiskCalcReqDto reqDto) {
-    	PayRiskCalcResDto resDto = new PayRiskCalcResDto();
-    	resDto.setRiskFactor("HIGH");
- 		return new ResponseEntity<PayRiskCalcResDto>(resDto, HttpStatus.OK);
- 	}
+		// TODO make calls to pre-validation APIs here and check if inputs are correct.
+		PayRiskCalcResDto resDto = new PayRiskCalcResDto();
+		resDto.setRiskFactor("HIGH");
+		return new ResponseEntity<PayRiskCalcResDto>(resDto, HttpStatus.OK);
+	}
 }
