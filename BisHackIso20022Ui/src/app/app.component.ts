9bd2ee3199af2e65228b07fa7a63f992aaa81bb9
Akshay Ahluwalia <akshayahluwalia@Akshays-MacBook-Air.local>
2021-03-17 14:17:51 -0400

    update to ui. will need to complete on the vm to finalize

--- BisHackIso20022Ui/src/app/app.component.ts
+++ BisHackIso20022Ui/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BisHackIso20022Ui';
+
+  formData = this.formBuilder.group({
+    institutionName: '',
+    creditorAccount: '',
+    creditorName: '',
+    amount: '',
+    debitorAccount: ''
+  })
+
+  constructor (private formBuilder: FormBuilder, public httpClient: HttpClient
+    ) {}
+
+    onSubmit(): void {
+      this.formData.value.uuid = "aye2yes-" + uuid.v4();
+      console.log(this.formData);
+
+      let response = this.httpClient.post<any>("http://localhost:8080/paymentRisk", this.formData.value).pipe(
+
+      );
+      console.log(response);
+    }
+
 }
