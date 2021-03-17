3a48fc63e865d30f9b1a4053202443a60790336c
Kian Nejadfard <k.nejadfard@gmail.com>
2021-03-17 11:47:16 -0400

    Add debitor account format prevalidation call

--- BisHackIso20022Api/src/main/java/com/bishack/api/service/SwiftValidationDataAgg.java
+++ BisHackIso20022Api/src/main/java/com/bishack/api/service/SwiftValidationDataAgg.java
@@ -29,58 +29,62 @@ public class SwiftValidationDataAgg implements IRiskEngineDataAgg {
         List<TrxRatingModelRequestDto> trxRatingModelRequestDtos = new ArrayList<>();
         TrxRecord record = translateToTrxRecord(payRiskCalcReqDto);
 
+        VerifyAccountFmtRespDto verifyCreditorAccountFmtResponse = null;
         try {
-            VerifyAccountFmtRespDto verifyCreditorAccountFmtResponse = null;
-            try {
-                verifyCreditorAccountFmtResponse = prevalidateAccountFormat(record);
-                LOG.debug("Account format verification result: {}", verifyCreditorAccountFmtResponse);
-            } catch (Exception e) {
-                LOG.warn("Swift call encountered exception: {}", e);
-                // For demo we don't want to bother with handling this. Rating model is somewhat mocked so
-                // it won't cause issues.
-            }
-
-            VerifyAccountRespDto verifyCreditorAccountResponse = null;
-            try {
-                verifyCreditorAccountResponse = prevalidateAccount(record);
-                LOG.debug("Account verification result: {}", verifyCreditorAccountResponse);
-            } catch (Exception e) {
-                LOG.warn("Swift call encountered exception: {}", e);
-                // For demo we don't want to bother with handling this. Rating model is somewhat mocked so
-                // it won't cause issues.
-            }
-
-            // Translate to TrxRatingModelRequestDto instances if applicable.
-            // This is hardcoded for demo.
-            if ("500105170123456789".equals(payRiskCalcReqDto.getCreditorAccount())) {
-                TrxRatingModelRequestDto acctFormatRating = new TrxRatingModelRequestDto();
-                acctFormatRating.setAttrName("AC_FORMAT_VALIDATION");
-                acctFormatRating.setCategory("SWIFT_VALIDATION");
-                acctFormatRating.setValue("LOW");
-                trxRatingModelRequestDtos.add(acctFormatRating);
-
-                TrxRatingModelRequestDto acctRating = new TrxRatingModelRequestDto();
-                acctRating.setAttrName("AC_VALIDATION");
-                acctRating.setCategory("SWIFT_VALIDATION");
-                acctRating.setValue("LOW");
-                trxRatingModelRequestDtos.add(acctRating);
-            } else if ("100000010123123123".equals(payRiskCalcReqDto.getCreditorAccount())) {
-                TrxRatingModelRequestDto acctFormatRating = new TrxRatingModelRequestDto();
-                acctFormatRating.setAttrName("AC_FORMAT_VALIDATION");
-                acctFormatRating.setCategory("SWIFT_VALIDATION");
-                acctFormatRating.setValue("MEDIUM");
-                trxRatingModelRequestDtos.add(acctFormatRating);
-
-                TrxRatingModelRequestDto acctRating = new TrxRatingModelRequestDto();
-                acctRating.setAttrName("AC_VALIDATION");
-                acctRating.setCategory("SWIFT_VALIDATION");
-                acctRating.setValue("MEDIUM");
-                trxRatingModelRequestDtos.add(acctRating);
-            }
+            verifyCreditorAccountFmtResponse = prevalidateCreditorAccountFormat(record);
+            LOG.debug("Creditor account format verification result: {}", verifyCreditorAccountFmtResponse);
         } catch (Exception e) {
-            LOG.warn("Encountered exception: {}", e);
-            // We want to ignore this exception for demo's sake. Since our rating model is somewhat mocked.
-            
+            LOG.warn("Swift call encountered exception: {}", e);
+            // For demo we don't want to bother with handling this. Rating model is somewhat mocked so
+            // it won't cause issues.
+        }
+
+        VerifyAccountFmtRespDto verifyDebitorAccountFmtResponse = null;
+        try {
+            verifyCreditorAccountFmtResponse = prevalidateDebitorAccountFormat(record);
+            LOG.debug("Debitor account format verification result: {}", verifyDebitorAccountFmtResponse);
+        } catch (Exception e) {
+            LOG.warn("Swift call encountered exception: {}", e);
+            // For demo we don't want to bother with handling this. Rating model is somewhat mocked so
+            // it won't cause issues.
+        }
+
+        VerifyAccountRespDto verifyCreditorAccountResponse = null;
+        try {
+            verifyCreditorAccountResponse = prevalidateCreditorAccount(record);
+            LOG.debug("Creditor account verification result: {}", verifyCreditorAccountResponse);
+        } catch (Exception e) {
+            LOG.warn("Swift call encountered exception: {}", e);
+            // For demo we don't want to bother with handling this. Rating model is somewhat mocked so
+            // it won't cause issues.
+        }
+
+        // Translate to TrxRatingModelRequestDto instances if applicable.
+        // This is hardcoded for demo.
+        if ("500105170123456789".equals(payRiskCalcReqDto.getCreditorAccount())) {
+            TrxRatingModelRequestDto acctFormatRating = new TrxRatingModelRequestDto();
+            acctFormatRating.setAttrName("AC_FORMAT_VALIDATION");
+            acctFormatRating.setCategory("SWIFT_VALIDATION");
+            acctFormatRating.setValue("LOW");
+            trxRatingModelRequestDtos.add(acctFormatRating);
+
+            TrxRatingModelRequestDto acctRating = new TrxRatingModelRequestDto();
+            acctRating.setAttrName("AC_VALIDATION");
+            acctRating.setCategory("SWIFT_VALIDATION");
+            acctRating.setValue("LOW");
+            trxRatingModelRequestDtos.add(acctRating);
+        } else if ("100000010123123123".equals(payRiskCalcReqDto.getCreditorAccount())) {
+            TrxRatingModelRequestDto acctFormatRating = new TrxRatingModelRequestDto();
+            acctFormatRating.setAttrName("AC_FORMAT_VALIDATION");
+            acctFormatRating.setCategory("SWIFT_VALIDATION");
+            acctFormatRating.setValue("MEDIUM");
+            trxRatingModelRequestDtos.add(acctFormatRating);
+
+            TrxRatingModelRequestDto acctRating = new TrxRatingModelRequestDto();
+            acctRating.setAttrName("AC_VALIDATION");
+            acctRating.setCategory("SWIFT_VALIDATION");
+            acctRating.setValue("MEDIUM");
+            trxRatingModelRequestDtos.add(acctRating);
         }
 
         return trxRatingModelRequestDtos;
@@ -90,34 +94,34 @@ public class SwiftValidationDataAgg implements IRiskEngineDataAgg {
         TrxRecord record = new TrxRecord();
         record.setCreditorName(req.getCreditorName());
         record.setCreditorAccount(req.getCreditorAccount());
-        record.setInstitutionName(req.getInstitutionName());
+        record.setCreditorInstitutionName(req.getInstitutionName());
         record.setUuid(UUID.randomUUID().toString());
         record.setAmount(req.getAmount());
 
         // TODO hardcoded for demo.
         if ("500105170123456789".equals(req.getCreditorAccount())) {
-            record.setIban("DE41500105170123456789");
-            record.setCountryCode("DE");
-            record.setBankId("50010517");
-            record.setBankIdCode("INGDDEFFXXX");
+            record.setCreditorIban("DE41500105170123456789");
+            record.setCreditorCountryCode("DE");
+            record.setCreditorBankId("50010517");
+            record.setCreditorBankIdCode("INGDDEFFXXX");
         } else if ("100000010123123123".equals(req.getCreditorAccount())) {
-            record.setIban("DE39100000010123123123");
-            record.setCountryCode("DE");
-            record.setBankId("10000001");
-            record.setBankIdCode("BLKFDE33");
+            record.setCreditorIban("DE39100000010123123123");
+            record.setCreditorCountryCode("DE");
+            record.setCreditorBankId("10000001");
+            record.setCreditorBankIdCode("BLKFDE33");
         }
         return record;
     }
 
-    private VerifyAccountFmtRespDto prevalidateAccountFormat(TrxRecord record) throws Exception {
+    private VerifyAccountFmtRespDto prevalidateCreditorAccountFormat(TrxRecord record) throws Exception {
         VerifyAccountFmtReqDto prevalAccountFormatDto = new VerifyAccountFmtReqDto();
-        prevalAccountFormatDto.setAccount_identification(record.getIban());
-        prevalAccountFormatDto.setCountry_code(record.getCountryCode());
-        prevalAccountFormatDto.setFinancial_institution_identification(record.getInstitutionId());
+        prevalAccountFormatDto.setAccount_identification(record.getCreditorIban());
+        prevalAccountFormatDto.setCountry_code(record.getCreditorCountryCode());
+        prevalAccountFormatDto.setFinancial_institution_identification(record.getCreditorInstitutionId());
         return swiftApiService.swiftPrevalAcFormat(prevalAccountFormatDto);
     }
 
-    private VerifyAccountRespDto prevalidateAccount(TrxRecord record) throws Exception {
+    private VerifyAccountRespDto prevalidateCreditorAccount(TrxRecord record) throws Exception {
         VerifyAccountReqDto dto = new VerifyAccountReqDto();
         dto.setCreditorName(record.getCreditorName());
         // Request is in the scope of the payment initiation
@@ -126,6 +130,14 @@ public class SwiftValidationDataAgg implements IRiskEngineDataAgg {
         dto.setCreditorName(record.getCreditorName());
         dto.setCorrelationIdentifier("SCENARIO1-CORRID-001");
         return swiftApiService.swiftPrevalAcVerify(dto, "916a97d-a699-4f20-b8c2-2b07e2684a27");
+    }
+
+    private VerifyAccountFmtRespDto prevalidateDebitorAccountFormat(TrxRecord record) throws Exception {
+        VerifyAccountFmtReqDto prevalAccountFormatDto = new VerifyAccountFmtReqDto();
+        prevalAccountFormatDto.setAccount_identification(record.getDebitorIban());
+        prevalAccountFormatDto.setCountry_code(record.getDebitorCountryCode());
+        prevalAccountFormatDto.setFinancial_institution_identification(record.getDebitorInstitutionId());
+        return swiftApiService.swiftPrevalAcFormat(prevalAccountFormatDto);
     }
 
 }
