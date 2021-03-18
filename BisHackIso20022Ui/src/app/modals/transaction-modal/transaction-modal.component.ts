5d36c3e06337012a06901bf1a77bf15ddcf840cc
Akshay Ahluwalia <akshayahluwalia@Akshays-MacBook-Air.local>
2021-03-18 06:02:13 -0400

    fix for issue where not waiting for response

--- BisHackIso20022Ui/src/app/modals/transaction-modal/transaction-modal.component.ts
+++ BisHackIso20022Ui/src/app/modals/transaction-modal/transaction-modal.component.ts
@@ -42,83 +42,76 @@ export class TransactionModalComponent {
       console.log(res);
       this.responseData = res;
 
-    });
-    /*this.responseData = {trxRatingModelRequestDtos:[{category:"CURRENT_TRX",attrName:"BENEFICIARY_AC",value:"500105170123456789"},
-      {category:"CURRENT_TRX",attrName:"SRC_AC",value:"123110040000109876543210"},
-      {category:"CURRENT_TRX",attrName:"AMOUNT",value:"1500"},
-      {category:"INTERNAL_COMPLIANCE",attrName:"HIGH_RISK_BENE_CNTRY",value:"BA,BB"},
-      {category:"INTERNAL_TRX_HIST",attrName:"SRC_AVG_TRX_AMOUNT",value:"1500"},
-      {category:"INTERNAL_TRX_HIST",attrName:"SRC_TOTAL_TRX_AMOUNT",value:"100000"},
-      {category:"SWIFT_COMPLIANCE",attrName:"BENEFICIARY_AC_VERIFICATION",value:"LOW"},
-      {category:"SWIFT_COMPLIANCE",attrName:"SRC_AC_VERIFICATION",value:"LOW"},
-      {category:"SWIFT_VALIDATION",attrName:"BENEFICIARY_AC_FORMAT",value:"LOW"},
-      {category:"SWIFT_VALIDATION",attrName:"SRC_AC_FORMAT",value:"LOW"}]}*/
-    this.responseData.riskRatingDetails.mlModelInput.trxRatingModelRequestDtos.forEach(element => {
-      if(element.category == "CURRENT_TRX" && element.attrName == "BENEFICIARY_AC")
-      {
-        this.currentTrxBen = element.value;
-      }
-      if(element.category == "CURRENT_TRX" && element.attrName == "SRC_AC")
-      {
-        this.currentTrxSrc = element.value;
-      }
-      if(element.category == "CURRENT_TRX" && element.attrName == "AMOUNT")
-      {
-        this.currentTrxAmm = element.value;
-      }
-      if(element.category == "INTERNAL_COMPLIANCE" && element.attrName == "HIGH_RISK_BENE_CNTRY")
-      {
-        this.internalComplianceHRBC = element.value;
-      }
-      if(element.category == "INTERNAL_TRX_HIST" && element.attrName == "SRC_AVG_TRX_AMOUNT")
-      {
-        this.internalTranHistSATA = element.value;
-      }
-      if(element.category == "INTERNAL_TRX_HIST" && element.attrName == "SRC_TOTAL_TRX_AMOUNT")
-      {
-        this.internalTranHistSTTA = element.value;
-      }
-      if(element.category == "SWIFT_COMPLIANCE" && element.attrName == "BENEFICIARY_AC_VERIFICATION")
-      {
-        this.swiftComplianceBAV = element.value;
-      }
-      if(element.category == "SWIFT_COMPLIANCE" && element.attrName == "SRC_AC_VERIFICATION")
-      {
-        this.swiftComplianceSAV = element.value;
-      }
-      if(element.category == "SWIFT_VALIDATION" && element.attrName == "BENEFICIARY_AC_FORMAT")
-      {
-        this.swiftValidationBAF = element.value;
-      }
-      if(element.category == "SWIFT_VALIDATION" && element.attrName == "SRC_AC_FORMAT")
-      {
-        this.swiftValidationSAF = element.value;
-      }
+      this.responseData.riskRatingDetails.mlModelInput.trxRatingModelRequestDtos.forEach(element => {
+        if(element.category == "CURRENT_TRX" && element.attrName == "BENEFICIARY_AC")
+        {
+          this.currentTrxBen = element.value;
+        }
+        if(element.category == "CURRENT_TRX" && element.attrName == "SRC_AC")
+        {
+          this.currentTrxSrc = element.value;
+        }
+        if(element.category == "CURRENT_TRX" && element.attrName == "AMOUNT")
+        {
+          this.currentTrxAmm = element.value;
+        }
+        if(element.category == "INTERNAL_COMPLIANCE" && element.attrName == "HIGH_RISK_BENE_CNTRY")
+        {
+          this.internalComplianceHRBC = element.value;
+        }
+        if(element.category == "INTERNAL_TRX_HIST" && element.attrName == "SRC_AVG_TRX_AMOUNT")
+        {
+          this.internalTranHistSATA = element.value;
+        }
+        if(element.category == "INTERNAL_TRX_HIST" && element.attrName == "SRC_TOTAL_TRX_AMOUNT")
+        {
+          this.internalTranHistSTTA = element.value;
+        }
+        if(element.category == "SWIFT_COMPLIANCE" && element.attrName == "BENEFICIARY_AC_VERIFICATION")
+        {
+          this.swiftComplianceBAV = element.value;
+        }
+        if(element.category == "SWIFT_COMPLIANCE" && element.attrName == "SRC_AC_VERIFICATION")
+        {
+          this.swiftComplianceSAV = element.value;
+        }
+        if(element.category == "SWIFT_VALIDATION" && element.attrName == "BENEFICIARY_AC_FORMAT")
+        {
+          this.swiftValidationBAF = element.value;
+        }
+        if(element.category == "SWIFT_VALIDATION" && element.attrName == "SRC_AC_FORMAT")
+        {
+          this.swiftValidationSAF = element.value;
+        }
+
+      });
+
+      this.responseData.riskRatingDetails.riskRatingDetails.forEach(element => {
+        if(element.category == "SWIFT_VALIDATION")
+        {
+          this.swiftValidationScore = element.score;
+        }
+        if(element.category == "SWIFT_COMPLIANCE")
+        {
+          this.swiftComplianceScore = element.score;
+        }
+        if(element.category == "INTERNAL_COMPLIANCE")
+        {
+          this.internalComplianceScore = element.score;
+        }
+        if(element.category == "INTERNAL_TRX_HIST")
+        {
+          this.internalTranHistScore = element.score;
+        }
+        if(element.category == "OVERALL_SCORE")
+        {
+          this.overallScore = element.score;
+        }
+      });
 
     });
 
-    this.responseData.riskRatingDetails.riskRatingDetails.forEach(element => {
-      if(element.category == "SWIFT_VALIDATION")
-      {
-        this.swiftValidationScore = element.score;
-      }
-      if(element.category == "SWIFT_COMPLIANCE")
-      {
-        this.swiftComplianceScore = element.score;
-      }
-      if(element.category == "INTERNAL_COMPLIANCE")
-      {
-        this.internalComplianceScore = element.score;
-      }
-      if(element.category == "INTERNAL_TRX_HIST")
-      {
-        this.internalTranHistScore = element.score;
-      }
-      if(element.category == "OVERALL_SCORE")
-      {
-        this.overallScore = element.score;
-      }
-    });
+
 
 
 
