cd62085ef1431b5c09125a92224c03483f715943
Akshay Ahluwalia <akshayahluwalia@Akshays-MacBook-Air.local>
2021-03-19 00:23:57 -0400

    added spinning loading thing

--- BisHackIso20022Ui/src/app/app.module.ts
+++ BisHackIso20022Ui/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatTableModule } from '@angular/material/table'
 import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { TransactionModalComponent } from './modals/transaction-modal/transactio
     MatTableModule,
     MatDialogModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
